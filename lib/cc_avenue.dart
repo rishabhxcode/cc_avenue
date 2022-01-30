f562cbdef5167118375f7c59b88694a0f30e931b
rishabhxcode <rishabhxcode@gmail.com>
2022-01-30 21:26:10 +0530

    made null safe

--- lib/cc_avenue.dart
+++ lib/cc_avenue.dart
@@ -55,8 +55,8 @@ import 'package:flutter/services.dart';
 class CcAvenue {
   static const MethodChannel _channel = const MethodChannel('cc_avenue');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
+  static Future<String?> get platformVersion async {
+    final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 ///[cCAvenueInit] Initialized the field requried for Payment GateWay
@@ -70,15 +70,15 @@ class CcAvenue {
 /// [cancelUrl] when the user cancel the request
 /// [redirectUrl] After the Transaction it will redirect and shows the status either Success or Failed
   static Future<void> cCAvenueInit({
-    String transUrl,
-    String rsaKeyUrl,
-    String accessCode,
-    String merchantId,
-    String orderId,
-    String currencyType,
-    String amount,
-    String cancelUrl,
-    String redirectUrl,
+    String? transUrl,
+    String? rsaKeyUrl,
+    String? accessCode,
+    String? merchantId,
+    String? orderId,
+    String? currencyType,
+    String? amount,
+    String? cancelUrl,
+    String? redirectUrl,
   }) async {
     await _channel.invokeMethod('CC_Avenue', {
       'transUrl': transUrl ?? '',
